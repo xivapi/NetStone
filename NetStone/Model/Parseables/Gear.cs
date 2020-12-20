1a8e8d50c52553d2924a88aad21626d1161e3a24
goat <16760685+goaaats@users.noreply.github.com>
2020-12-21 00:14:24 +0100

    docs: gear xmldoc

--- NetStone/Model/Parseables/Gear.cs
+++ NetStone/Model/Parseables/Gear.cs
@@ -16,45 +16,87 @@ namespace NetStone.Model.Parseables
             this.definition = definition;
         }
 
+        /// <summary>
+        /// Information about the characters' weapon. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Weapon => new GearEntry(this.RootNode, this.definition.Weapon).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' shield/offhand. Null if none equipped.
+        /// </summary>
         [CanBeNull]
-        public GearEntry Shield => new GearEntry(this.RootNode, this.definition.Shield).GetOptional();
+        public GearEntry Offhand => new GearEntry(this.RootNode, this.definition.Shield).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' headgear. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Head => new GearEntry(this.RootNode, this.definition.Head).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' body gear. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Body => new GearEntry(this.RootNode, this.definition.Body).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' gloves. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Hands => new GearEntry(this.RootNode, this.definition.Hands).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' waist gear. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Waist => new GearEntry(this.RootNode, this.definition.Waist).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' pants. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Legs => new GearEntry(this.RootNode, this.definition.Legs).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' shoes. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Feet => new GearEntry(this.RootNode, this.definition.Feet).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' earrings. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Earrings => new GearEntry(this.RootNode, this.definition.Earrings).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' necklace. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Necklace => new GearEntry(this.RootNode, this.definition.Necklace).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' bracelets. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Bracelets => new GearEntry(this.RootNode, this.definition.Bracelets).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' first ring. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Ring1 => new GearEntry(this.RootNode, this.definition.Ring1).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' second ring. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public GearEntry Ring2 => new GearEntry(this.RootNode, this.definition.Ring2).GetOptional();
 
+        /// <summary>
+        /// Information about the characters' soul crystal. Null if none equipped.
+        /// </summary>
         [CanBeNull]
         public SoulcrystalEntry Soulcrystal => new SoulcrystalEntry(this.RootNode, this.definition.Soulcrystal).GetOptional();
 
