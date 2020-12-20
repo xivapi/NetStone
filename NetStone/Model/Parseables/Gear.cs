630b2e0bbd1ea8a299e183312e47e1d61714f121
goat <16760685+goaaats@users.noreply.github.com>
2020-12-21 00:11:26 +0100

    docs: add CanBeNull to Gear

--- NetStone/Model/Parseables/Gear.cs
+++ NetStone/Model/Parseables/Gear.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using HtmlAgilityPack;
+using JetBrains.Annotations;
 using NetStone.Definitions.Model;
 
 namespace NetStone.Model.Parseables
@@ -15,32 +16,46 @@ namespace NetStone.Model.Parseables
             this.definition = definition;
         }
 
+        [CanBeNull]
         public GearEntry Weapon => new GearEntry(this.RootNode, this.definition.Weapon).GetOptional();
 
+        [CanBeNull]
         public GearEntry Shield => new GearEntry(this.RootNode, this.definition.Shield).GetOptional();
 
+        [CanBeNull]
         public GearEntry Head => new GearEntry(this.RootNode, this.definition.Head).GetOptional();
 
+        [CanBeNull]
         public GearEntry Body => new GearEntry(this.RootNode, this.definition.Body).GetOptional();
 
+        [CanBeNull]
         public GearEntry Hands => new GearEntry(this.RootNode, this.definition.Hands).GetOptional();
 
+        [CanBeNull]
         public GearEntry Waist => new GearEntry(this.RootNode, this.definition.Waist).GetOptional();
 
+        [CanBeNull]
         public GearEntry Legs => new GearEntry(this.RootNode, this.definition.Legs).GetOptional();
 
+        [CanBeNull]
         public GearEntry Feet => new GearEntry(this.RootNode, this.definition.Feet).GetOptional();
 
+        [CanBeNull]
         public GearEntry Earrings => new GearEntry(this.RootNode, this.definition.Earrings).GetOptional();
 
+        [CanBeNull]
         public GearEntry Necklace => new GearEntry(this.RootNode, this.definition.Necklace).GetOptional();
 
+        [CanBeNull]
         public GearEntry Bracelets => new GearEntry(this.RootNode, this.definition.Bracelets).GetOptional();
 
+        [CanBeNull]
         public GearEntry Ring1 => new GearEntry(this.RootNode, this.definition.Ring1).GetOptional();
 
+        [CanBeNull]
         public GearEntry Ring2 => new GearEntry(this.RootNode, this.definition.Ring2).GetOptional();
 
+        [CanBeNull]
         public SoulcrystalEntry Soulcrystal => new SoulcrystalEntry(this.RootNode, this.definition.Soulcrystal).GetOptional();
 
     }
