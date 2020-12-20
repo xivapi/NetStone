ccbdc10bfcb3aa05ad329d4e0b009bde90296675
goat <16760685+goaaats@users.noreply.github.com>
2020-12-21 00:43:10 +0100

    feat: IPaginatedResult interface

---
+++ NetStone/Model/IPaginatedResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetStone.Model
+{
+    interface IPaginatedResult <T> where T : LodestoneParseable
+    {
+        int CurrentPage { get; set; }
+        int NumPages { get; set; }
+
+        Task<T> GetNextPage();
+    }
+}
